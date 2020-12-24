08b30e96bb1b27a80d25775469b851b6aa7ac64d
FireRedz <asiagamingz122@gmail.com>
2020-12-24 16:44:52 +0800

    change dir to package looking thing idk, also added logging

---
+++ osr2png/objects/image/classes/template.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+class Template:
+    def __init__(self, img, settings):
+        self.img = img
+        self.settings = settings
+
+
+        self.loadImage()
+        self.addToImage()
+        #print(f'x is added! (broken lol)')
+
+    def loadImage(self):
+        pass
+
+    def addToImage(self):
+        pass
+
+    
---
+++ osr2png/utils/__init__.py
@@ -0,0 +1,34 @@
+import json, os
+from types import SimpleNamespace
+
+def getSizeMultiplier(height):
+    return height/720 # HOW IS THIS WORKING
+
+def getSizeMultiplier_(pixel: int):
+    ''' this is retarded how tf am i supposed to calc resize shit '''
+    defaultPixel = 921600 # 1280x720
+    return (pixel/defaultPixel)*1
+
+
+
+def loadConfig():
+    if not os.path.isfile('config.json'):
+        raise Exception('No config found!')
+    with open('config.json') as file:
+        res = json.loads(file.read(), object_hook=lambda d: SimpleNamespace(**d))
+
+    return res
+
+
+def saveConfig(settings):
+    with open('config.json', 'w') as f:
+        f.write(json.dumps(settings.__dict__, indent=4))
+
+    print('Config Saved!')
+
+
+def checkFolder(settings):
+    if not os.path.isdir(settings.outdir):
+        os.mkdir(settings.outdir)
+    if not os.path.isdir('cache'):
+        os.mkdir('cache')
